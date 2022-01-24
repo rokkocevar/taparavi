1d7c382c18993f97ee2d970d251293e8c59830d8
rokkocevar <rokkocevar07@gmail.com>
2022-01-24 13:36:25 +0100

    username

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,8 @@ module ApplicationHelper
   def display_navbar_options
     capture do
       if user_signed_in?
+        concat link_to "Uredi profil", edit_user_registration_path
+        concat " "
         concat link_to "odjava", destroy_user_session_path, method: :delete
       else
         concat link_to "prijava", new_user_session_path
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,11 +1,12 @@
 module PostsHelper
   def display_post_show_links(post)
-    html = ""
-    if current_user == post.user
-    html = link_to 'Edit', edit_post_path(post)
-    html += ' | '
+    capture do
+      if current_user == post.user
+        concat link_to 'Edit', edit_post_path(post)
+        concat ' | '
     end
-    html += link_to 'Back', posts_path
-    html.html_safe
+
+      concat link_to 'Back', posts_path
   end
+end
 end
