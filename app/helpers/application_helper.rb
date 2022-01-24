fb1bed756efb9a00efa8e31c02c1f4f76213c8c6
rokkocevar <rokkocevar07@gmail.com>
2022-01-24 12:52:04 +0100

    authenticate navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_navbar_options
+    html =link_to "domov", root_path
+    html +=link_to "prijava", new_user_session_path if !user_signed_in?
+    html +=link_to "registracija", new_user_registration_path if !user_signed_in?
+    html +=link_to "odjava", destroy_user_session_path, method: :delete if user_signed_in?
+    html.html_safe
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,11 @@
 module PostsHelper
+  def display_post_show_links(post)
+    html = ""
+    if current_user == post.user
+    html = link_to 'Edit', edit_post_path(post)
+    html += ' | '
+    end
+    html += link_to 'Back', posts_path
+    html.html_safe
+  end
 end
