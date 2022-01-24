a24c46f0bcdb5e325d1059021a22832f5c72194b
rokkocevar <rokkocevar07@gmail.com>
2022-01-24 13:10:09 +0100

    refaktoiranje helperjev

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,14 @@
 module ApplicationHelper
   def display_navbar_options
-    html =link_to "domov", root_path
-    html +=link_to "prijava", new_user_session_path if !user_signed_in?
-    html +=link_to "registracija", new_user_registration_path if !user_signed_in?
-    html +=link_to "odjava", destroy_user_session_path, method: :delete if user_signed_in?
-    html.html_safe
-  end
+    capture do
+      if user_signed_in?
+        concat link_to "odjava", destroy_user_session_path, method: :delete
+      else
+        concat link_to "prijava", new_user_session_path
+        concat " "
+        concat link_to "registracija", new_user_registration_path
+        concat " "
+    end
+   end
+ end
 end
