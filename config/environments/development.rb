ea4edceee6bdb44ad5d0a229bca15c01d5f353f2
rokkocevar <rokkocevar07@gmail.com>
2022-05-04 21:47:32 +0200

    kfehsdddspi

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,8 +17,8 @@ Rails.application.configure do
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
-
-    config.action_controller.enable_fragment_cache_logging = true
+    config.action_controller.perform_caching = false
+    config.action_controller.enable_fragment_cache_logging = false
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon_dev
-  config.webpacker.check_yarn_integrity = false
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
